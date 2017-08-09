76dc9b4b0826f3b9378baf5c2dcdc302bc4f7392
chenshipeng <858753795@qq.com>
2017-08-09 14:52:16 +0800

    setup
    setup for the project

---
+++ ManageYourStars/ManageYourStars/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ManageYourStars
+//
+//  Created by 陈仕鹏 on 2017/8/9.
+//  Copyright © 2017年 csp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
